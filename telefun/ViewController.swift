b53d68aeab9fd21eacc465168db9b76e7072141d
o1iviachen <olivia63chen@gmail.com>
2025-05-23 14:08:14 -0400

    Initial Commit

---
+++ telefun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  telefun
+//
+//  Created by olivia chen on 2025-05-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
